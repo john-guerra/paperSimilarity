f2142bed7697b917f3fcbc00a849d001108d2ef3
John Alexis Guerra Gómez <john.guerra@gmail.com>
2024-08-02 16:23:03 -0700

    Bugfixes on authors comparison

---
+++ src/lib/components/PromiseStatus.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let promise = Promise.resolve([]);
+  console.log("⏳ promise status", promise);
+</script>
+
+{#await promise}
+  <span title="Promise loading">⏳</span>
+{:then response}
+  <span title="Got data {response?.length}">✅</span>
+{:catch error}
+  <span title={error}>🚫</span>
+{/await}
